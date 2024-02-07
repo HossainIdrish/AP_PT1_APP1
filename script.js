d704a5dfb07eae70b48e4dbdb368a63de678301c
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-02-06 21:12:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 var mymap = L.map('mapid').setView([38.6562, -90.3052], 12);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 20,
+	maxZoom: 25,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(mymap);
 
